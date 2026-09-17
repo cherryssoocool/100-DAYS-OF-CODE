8f7618f2eed6e3143617b15dfe380ab2fbc9d016
cherryssoocool <cherril.40896@stu.upes.ac.in>
2026-09-17 20:21:44 +0000

    Day09

---
+++ Day09/q2.c
@@ -0,0 +1,51 @@
+/*
+ * Name :  Cherril
+ * Roll : 590040896
+ * Day : 09 Question: 2
+ * Date : 18-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Q18: Write a program that accepts a percentage (0-100)
+ and assigns a grade based on the following criteria:
+ 90-100: Grade A
+ 80-89: Grade B
+ 70-79: Grade C
+ 60-69: Grade D
+ Below 60: Grade F.
+ */
+#include <stdio.h>
+
+int main()
+{
+    float percentage;
+
+    printf("Enter percentage: ");
+    scanf("%f", &percentage);
+
+    if (percentage >= 90 && percentage <= 100)
+    {
+        printf("Grade A");
+    }
+    else if (percentage >= 80)
+    {
+        printf("Grade B");
+    }
+    else if (percentage >= 70)
+    {
+        printf("Grade C");
+    }
+    else if (percentage >= 60)
+    {
+        printf("Grade D");
+    }
+    else if (percentage >= 0)
+    {
+        printf("Grade F");
+    }
+    else
+    {
+        printf("Invalid percentage");
+    }
+
+    return 0;
+}
